4421bea642220163b65333f118799ff509645a3c
Hung Tran <hungtran@hungs-mbp.home>
2022-11-11 14:02:10 +0000

    format

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -1,5 +1,5 @@
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }: { url: string }): Response {
-    const tagVersion: string = __CIRCLE_CI_VERSION__;
-    return new Response(`Application is on tag version: ${tagVersion}`);
+	const tagVersion: string = __CIRCLE_CI_VERSION__;
+	return new Response(`Application is on tag version: ${tagVersion}`);
 }
