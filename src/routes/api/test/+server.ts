d8f43477f2fb6617469d20b039019152b796697a
Hung Tran <hungtran@hungs-mbp.home>
2022-11-12 09:54:15 +0000

    add linting failure

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -3,5 +3,8 @@ import { CIRCLE_WORKFLOW_ID } from '$env/static/private';
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }: { url: string }): Response {
 	const tagVersion: string = CIRCLE_WORKFLOW_ID;
-	return new Response(`Application is on tag version: ${tagVersoin}`);
+	if (false) {
+		console.log('hello');
+	}
+	return new Response(`Application is on tag version: ${tagVersion}`);
 }
