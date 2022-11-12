6d11069d26772f7ac5129ddd194f34bbfd95238e
Hung Tran <hungtran@hungs-mbp.home>
2022-11-12 09:36:34 +0000

    add syntax error

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -2,6 +2,7 @@ import { CIRCLE_WORKFLOW_ID } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }: { url: string }): Response {
+	dfd
 	const tagVersion: string = CIRCLE_WORKFLOW_ID;
 	return new Response(`Application is on tag version: ${tagVersion}`);
 }
