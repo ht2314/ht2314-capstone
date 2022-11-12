957aa3b8a68760b263108847790ee5598b3a498d
Hung Tran <hungtran@hungs-mbp.home>
2022-11-12 09:55:48 +0000

    revert changes

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -3,8 +3,5 @@ import { CIRCLE_WORKFLOW_ID } from '$env/static/private';
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }: { url: string }): Response {
 	const tagVersion: string = CIRCLE_WORKFLOW_ID;
-	if (false) {
-		console.log('hello');
-	}
 	return new Response(`Application is on tag version: ${tagVersion}`);
 }
